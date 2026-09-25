0c108bc9d33451a59815489ac0261f3420fcb36a
HiteshRohira <hiteshrohira15@gmail.com>
2026-09-25 16:51:33 +0530

    Keep Blob SDK external to Next server bundle

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    serverComponentsExternalPackages: ["@vercel/blob"],
+  },
 };
 
 export default nextConfig;
